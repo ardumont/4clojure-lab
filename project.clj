105b382c8b0c555445e9d416ba4cf43579faeb37
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-15 20:09:06 +0100

    Update the dependencies.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject my-4clojure-lab "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure  "1.3.0"]]
+  :dev-dependencies [[midje                "1.3.0-RC4"]
+                     [lein-midje           "1.0.7"]])
