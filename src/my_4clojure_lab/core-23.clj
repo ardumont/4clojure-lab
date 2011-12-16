f9b45834480f29ad246dcf124fa4788ccab9051c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-16 01:32:20 +0100

    Exo 23.

--- src/my_4clojure_lab/core-23.clj
+++ src/my_4clojure_lab/core-23.clj
@@ -9,11 +9,12 @@
 
 (println "--------- BEGIN 23  ----------" (java.util.Date.))
 
-test not run	
-(= (__ [1 2 3 4 5]) [5 4 3 2 1])
-test not run	
-(= (__ (sorted-set 5 7 2 7)) '(7 5 2))
-test not run	
-(= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
+(defn rev "Reverse"
+  [s])
+
+(fact
+  (rev [1 2 3 4 5]) => [5 4 3 2 1]
+  (rev (sorted-set 5 7 2 7)) => '(7 5 2)
+  (rev [[1 2][3 4][5 6]]) => [[5 6][3 4][1 2]])
 
 (println "--------- END 23  ----------" (java.util.Date.))
