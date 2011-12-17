842158ea72afcc5372b11ac23814a77fcd3563dd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-17 11:37:03 +0100

    Begin exercise 32.

---
+++ src/my_4clojure_lab/core-32.clj
@@ -0,0 +1,20 @@
+(ns ^{:doc "http://www.4clojure.com/problem/32#prob-title"}
+  my-4clojure-lab.core-32
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet]))
+
+(println "--------- BEGIN 32  ----------" (java.util.Date.))
+
+;; Write a function which duplicates each element of a sequence.
+
+(defn dup "Duplicate each element of the sequence"
+  [s])
+
+(fact
+  (dup [1 2 3]) => '(1 1 2 2 3 3)
+  (dup [:a :a :b :b]) => '(:a :a :a :a :b :b :b :b)
+  (dup [[1 2] [3 4]]) => '([1 2] [1 2] [3 4] [3 4])
+  (dup [[1 2] [3 4]]) => '([1 2] [1 2] [3 4] [3 4]))
+
+(println "--------- END 32  ----------" (java.util.Date.))
