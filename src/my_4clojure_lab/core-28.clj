2df2076fd1da40b3a97c55db3d565b821c2eedd0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-17 10:52:01 +0100

    My-flatten implementation. There must be a subtle way.

---
+++ src/my_4clojure_lab/core-28.clj
@@ -0,0 +1,25 @@
+(ns ^{:doc "http://www.4clojure.com/problem/28#prob-title"}
+  my-4clojure-lab.core-28
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet]))
+
+(println "--------- BEGIN 28  ----------" (java.util.Date.))
+
+;; Write a function which flattens a sequence.
+
+(defn my-flatten "flatten implementation"
+  [s]
+  (reduce
+   #(if (sequential? %2)
+      (concat % (my-flatten %2))
+      (concat % [%2]))
+   []
+   s))
+
+(fact
+  (my-flatten '((1 2) 3 [4 [5 6]])) => '(1 2 3 4 5 6)
+  (my-flatten ["a" ["b"] "c"]) => '("a" "b" "c")
+  (my-flatten '((((:a))))) => '(:a))
+
+(println "--------- END 28  ----------" (java.util.Date.))
