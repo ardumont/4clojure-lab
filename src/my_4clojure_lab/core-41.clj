19c6f1113205cebadaf9c407565b8e738bab4d3b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-17 19:20:00 +0100

    At last, drop-nth implementation.

--- src/my_4clojure_lab/core-41.clj
+++ src/my_4clojure_lab/core-41.clj
@@ -2,20 +2,19 @@
   my-4clojure-lab.core-41
   (:use clojure.repl)
   (:use clojure.java.javadoc)
-  (:use [midje.sweet])
-  (:use [clojure.pprint :only [pp pprint]]))
+  (:use [midje.sweet]))
 
 ;; Write a function which drops every Nth item from a sequence.
 
 (println "--------- BEGIN 41  ----------" (java.util.Date.))
 
-(defn my-drop "drops every Nth item from a sequence."
-  [n s])
+(defn drop-nth "drops every Nth item from a sequence."
+  [s n]
+  (for [x (range 1 (inc (count s))) :when (not (zero? (rem x n)))] (s (dec x))))
 
 (fact
-  (my-drop [1 2 3 4 5 6 7 8] 3) => [1 2 4 5 7 8]
-  (my-drop [:a :b :c :d :e :f] 2) => [:a :c :e]
-  (my-drop [1 2 3 4 5 6] 4) => [1 2 3 5 6])
+  (drop-nth [1 2 3 4 5 6 7 8] 3)   => [1 2 4 5 7 8]
+  (drop-nth [:a :b :c :d :e :f] 2) => [:a :c :e]
+  (drop-nth [1 2 3 4 5 6] 4)       => [1 2 3 5 6])
 
 (println "--------- END 41  ----------" (java.util.Date.))
-
