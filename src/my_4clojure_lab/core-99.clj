53c9afc90fdf77175db9f4cefeb92a331c3551c9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-20 15:33:48 +0100

    First implementation.

---
+++ src/my_4clojure_lab/core-99.clj
@@ -0,0 +1,23 @@
+(ns ^{:doc "http://www.4clojure.com/problem/99#prob-title"}
+  my-4clojure-lab.core-99
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet]))
+
+(println "--------- BEGIN 99  ----------" (java.util.Date.))
+
+;; Write a function which multiplies two numbers and returns the result as a sequence of its digits.
+
+(defn pdt "Function which multiplies two numbers and returns the result as a sequence of its digits."
+  [a b]
+  (loop [n (* a b) acc nil]
+    (if (zero? n)
+      (vec acc)
+      (recur (quot n 10) (cons (rem n 10) acc)))))
+
+(fact
+  (pdt 1 1) => [1]
+  (pdt 99 9) => [8 9 1]
+  (pdt 999 99) => [9 8 9 0 1])
+
+(println "--------- END 99  ----------" (java.util.Date.))
