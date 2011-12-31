df7762cae6c01094c578ee420378a75788b8ffee
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-31 19:23:44 +0100

    Gcd.

---
+++ src/my_4clojure_lab/core-66.clj
@@ -0,0 +1,23 @@
+(ns ^{:doc "http://www.4clojure.com/problem/66#prob-title"}
+  my-4clojure-lab.core-66
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet]))
+
+(println "--------- BEGIN 66  ----------" (java.util.Date.))
+
+;; Given two integers, write a function which returns the greatest
+;; common divisor.
+
+(defn gcd "Greatest common divisor"
+  [a b]
+  (if (zero? b) a
+      (gcd b (mod a b))))
+
+(fact 
+  (gcd 2 4) => 2
+  (gcd 10 5) => 5
+  (gcd 5 7) => 1
+  (gcd 1023 858) => 33)
+
+(println "--------- END 66  ----------" (java.util.Date.))
