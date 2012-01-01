04674b54e438b7cb2fac96219c5f3bbef7bfb6d9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-01 13:45:05 +0100

    Begin beauty is symmetry exercise.

---
+++ src/my_4clojure_lab/core-96.clj
@@ -0,0 +1,39 @@
+(ns ^{:doc "http://www.4clojure.com/problem/96#prob-title"}
+  my-4clojure-lab.core-96
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet]))
+
+(println "--------- BEGIN 96  ----------" (java.util.Date.))
+
+;; Let us define a binary tree as "symmetric" if the left half of the
+;; tree is the mirror image of the right half of the tree. Write a
+;; predicate to determine whether or not a given binary tree is
+;; symmetric. (see To Tree, or not to Tree for a reminder on the tree
+;; representation we're using).
+
+(defn bin-tree-sym? ""
+  [t]
+    (or
+   (nil? t)
+   (and
+    (coll? t)
+    (= 3 (count t))
+    (= (nth t 1) (nth t 2))
+    (every? bin-tree-sym? (next t)))))
+
+(fact
+  (bin-tree-sym? '(:a (:b nil nil)
+                      (:b nil nil))) => true
+  (bin-tree-sym? '(:a (:b nil nil)
+                      nil)) => false
+  (bin-tree-sym? '(:a (:b nil nil)
+                      (:c nil nil))) => false
+  (bin-tree-sym? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                    [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]]) => true
+  (bin-tree-sym? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                  [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]]) => false
+  (bin-tree-sym? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                  [2 [3 nil [4 [6 nil nil] nil]] nil]]) => false)
+
+(println "--------- END 96  ----------" (java.util.Date.))
