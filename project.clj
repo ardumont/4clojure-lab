437e31baa3a607ad09bc7916e6a4a25629ced7e5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-03 00:02:05 +0100

    Add marginalia dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject my-4clojure-lab "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure  "1.3.0"]]
-  :dev-dependencies [[midje                "1.3.0-RC4"]
-                     [lein-midje           "1.0.7"]])
+  :description "My-4clojure-lab - reference my experimentation to solve the problems."
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[midje           "1.3.0-RC4"]
+                     [lein-midje      "1.0.7"]
+                     [lein-marginalia "0.6.0"]])
