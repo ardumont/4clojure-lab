13d57ee6c627fbb2f55a6c6af824e65503057317
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-02 13:55:20 +0100

    Init the 148th problem Big divide.

---
+++ src/my_4clojure_lab/core-148.clj
@@ -0,0 +1,30 @@
+(ns ^{:doc "http://www.4clojure.com/problem/148#prob-title"}
+  my-4clojure-lab.core-148
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet])
+  (:use [clojure.pprint :only [pprint]]))
+
+(println "--------- BEGIN 148  ----------" (java.util.Date.))
+
+;; Write a function which calculates the sum of all natural numbers
+;; under n (first argument) which are evenly divisible by at least one
+;; of a and b (second and third argument).
+;; Numbers a and b are guaranteed to be coprimes.
+
+;; Note: Some test cases have a very large n, so the most obvious
+;; solution will exceed the time limit.
+
+(defn big-divide ""
+  [a b c])
+
+(fact 
+  (big-divide 3 17 11) => 0
+  (big-divide 10 3 5) => 23
+  (big-divide 1000 3 5) => 233168
+  (str (big-divide 100000000 3 5)) => "2333333316666668"
+  (str (big-divide (* 10000 10000 10000) 7 11)) => "110389610389889610389610"
+  (str (big-divide (* 10000 10000 10000) 757 809)) => "1277732511922987429116"
+  (str (big-divide (* 10000 10000 1000) 1597 3571)) => "4530161696788274281")
+
+(println "--------- END 148  ----------" (java.util.Date.))
