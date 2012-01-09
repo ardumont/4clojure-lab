f7ef1a4d08a779da81fb193b2a0b156c74977b8a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-09 18:53:09 +0100

    Deal with the length of the sequence that needs to be >= 2.

--- src/my_4clojure_lab/core-53.clj
+++ src/my_4clojure_lab/core-53.clj
@@ -17,16 +17,21 @@
   (second
    (reduce
     (fn [[m v :as r] e]
-      (println r e (m (dec e)))
-       ;; if the previous key (dec e) exists then e became the new key and the
-       ;; value is the seq we read until now with e as the last
-       ;; element, else we reinit the map with e as key and the
-       ;; sequence we read is just e
-      [{e (if (m (dec e)) (conj (m (dec e)) e) #{e})}
+      ;;      (println r e (m (dec e)))
+      ;; if the previous key (dec e) exists then e became the new key and the
+      ;; value is the seq we read until now with e as the last
+      ;; element, else we reinit the map with e as key and the
+      ;; sequence we read is just e
+      [{e (if (m (dec e))
+            (conj (m (dec e)) e)
+            #{e})}
        ;; if we now have a sequence with a greater length, we take
        ;; this one, that is we retrieve the first value of vals from
        ;; the map and copy it inside the vector of the longest sequence
-       (if (<= (count v) (count (first (vals m)))) (first (vals m)) (into [] v))])
+       (let [f (first (vals m))]
+         (if (and (< (count v) (count f)) (<= 2 (count f)))
+           f
+           (into [] v)))])
     
     [{} []]
     s)))
