518fd59069bf4c23603b7b27f21a7c2a315e19da
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-11 12:52:48 +0100

    Filter perfect square and aggregates it into a string.

---
+++ src/my_4clojure_lab/core-74.clj
@@ -0,0 +1,24 @@
+(ns ^{:doc "http://www.4clojure.com/problem/74#prob-title"}
+  my-4clojure-lab.core-74
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet])
+  (:use [clojure.pprint :only [pprint]])
+  (:use [clojure.string :only (split join)]))
+
+(println "--------- BEGIN 74  ----------" (java.util.Date.))
+
+;; Given a string of comma separated integers, write a function which
+;; returns a new comma separated string that only contains the numbers
+;; which are perfect squares.
+
+(defn psquare ""
+  [s]
+  (let [q (fn [b] (set (map #(* % %) (range 1 b))))]
+    (join "," (filter #((q %) %) (map read-string (re-seq #"\d+" s))))))
+
+(fact
+  (psquare "4,5,6,7,8,9") => "4,9"
+  (psquare "15,16,25,36,37") => "16,25,36")
+
+(println "--------- END 74  ----------" (java.util.Date.))
