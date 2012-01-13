8b3c64055c726b83a4fdf1869cd1d55327f18bc3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-13 21:05:11 +0100

    Intermediary commit - Reduction implementations.

---
+++ src/my_4clojure_lab/core-60.clj
@@ -0,0 +1,26 @@
+(ns ^{:doc "http://www.4clojure.com/problem/60#prob-title"}
+  my-4clojure-lab.core-60
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet])
+  (:use [clojure.pprint :only [pprint]]))
+
+(println "--------- BEGIN 60  ----------" (java.util.Date.))
+
+;; Write a function which behaves like reduce, but returns each
+;; intermediate value of the reduction. Your function must accept
+;; either two or three arguments, and the return sequence must be
+;; lazy.
+
+(defn mred "Reductions implementation."
+  [g [f & r]]
+  (reduce #(conj % (g (last %) %2)) [f] r))
+
+(fact
+  (take 5 (mred + (range))) => [0 1 3 6 10])
+
+(future-fact
+ (mred conj [1] [2 3 4]) => [[1] [1 2] [1 2 3] [1 2 3 4]]
+ (last (mred * 2 [3 4 5])) => 120)
+
+(println "--------- END 60  ----------" (java.util.Date.))
