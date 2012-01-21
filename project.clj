ceec9b23b1158046b44609b5627601195c62d25b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-21 16:00:53 +0100

    Upgrade the dependencies and add lazytest.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject my-4clojure-lab "1.0.0-SNAPSHOT"
   :description "My-4clojure-lab - reference my experimentation to solve the problems."
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje           "1.3.0-RC4"]
+  :dev-dependencies [[midje           "1.3.1"]
                      [lein-midje      "1.0.7"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]])
+                     [lein-marginalia "0.7.0-SNAPSHOT"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
