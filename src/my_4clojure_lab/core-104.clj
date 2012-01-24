b1dc63f27951adc2344175af1d3806d94273a957
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-24 02:03:06 +0100

    Alignment.

--- src/my_4clojure_lab/core-104.clj
+++ src/my_4clojure_lab/core-104.clj
@@ -13,19 +13,19 @@
 (defn roman "Roman natural"
   [n]
   (cond
-   (<= 0 (- n 1000)) (str "M" (roman (- n 1000)))
+   (<= 0 (- n 1000)) (str "M"  (roman (- n 1000)))
    (<= 0 (- n 900))  (str "CM" (roman (- n 900)))
-   (<= 0 (- n 500))  (str "D" (roman (- n 500)))
+   (<= 0 (- n 500))  (str "D"  (roman (- n 500)))
    (<= 0 (- n 400))  (str "CD" (roman (- n 400)))
-   (<= 0 (- n 100))  (str "C" (roman (- n 100)))
+   (<= 0 (- n 100))  (str "C"  (roman (- n 100)))
    (<= 0 (- n 90))   (str "XC" (roman (- n 90)))
-   (<= 0 (- n 50))   (str "L" (roman (- n 50)))
-   (<= 0 (- n 40))   (str "XL"(roman (- n 40)))
-   (<= 0 (- n 10))   (str "X" (roman (- n 10)))
+   (<= 0 (- n 50))   (str "L"  (roman (- n 50)))
+   (<= 0 (- n 40))   (str "XL" (roman (- n 40)))
+   (<= 0 (- n 10))   (str "X"  (roman (- n 10)))
    (<= 0 (- n 9))    (str "IX" (roman (- n 9)))
-   (<= 0 (- n 5))    (str "V" (roman (- n 5)))
+   (<= 0 (- n 5))    (str "V"  (roman (- n 5)))
    (<= 0 (- n 4))    (str "IV" (roman (- n 4)))
-   (<= 0 (- n 1))    (str "I" (roman (- n 1)))))
+   (<= 0 (- n 1))    (str "I"  (roman (- n 1)))))
 
 ;.;. For every disciplined effort, there is a multiple reward. -- Rohn
 (fact
