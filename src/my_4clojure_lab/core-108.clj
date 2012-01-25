dae323d5d5a0e1de73b3abc5fc2881c4a3cc0796
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-25 17:43:14 +0100

    4clojure.com/108 - Lazy searching

---
+++ src/my_4clojure_lab/core-108.clj
@@ -0,0 +1,41 @@
+(ns ^{:doc "http://www.4clojure.com/problem/108#prob-title"}
+  my-4clojure-lab.core-108
+  (:use clojure.repl)
+  (:use clojure.java.javadoc)
+  (:use [midje.sweet]))
+
+(println "--------- BEGIN 108  ----------" (java.util.Date.))
+
+;; Given any number of sequences, each sorted from smallest to largest, find the smallest number which appears in each
+;; sequence. The sequences may be infinite, so be careful to search lazily.
+
+(defn sm "Find the smallest number which appears in each sequence."
+  [v & xv]
+  (first 
+   (drop-while nil?
+               (map
+                (fn [e]
+                  (if  (= (count xv)
+                          (count 
+                           (filter #(= e %)
+                                   (map
+                                    (fn [l]
+                                      (if (= e (first (drop-while #(> e %) l)))
+                                        e))
+                                    xv))))
+                    e))
+                v))))
+
+(fact
+  (sm [3 4 5]) => 3
+  (sm [1 2 3 4 5 6 7] [0.5 3/2 4 19 25 67 149] [1 3 4 19] [4 17]) => 4)
+
+(fact
+ (sm (range) (range 0 100 7/6) [2 3 5 7 11 13]) => 7)
+
+(fact
+  (sm (map #(* % % %) (range))
+      (filter #(zero? (bit-and % (dec %))) (range))
+      (iterate inc 20)) => 64)
+
+(println "--------- END 108  ----------" (java.util.Date.))
