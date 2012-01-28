4a74449cae15a321a9a6367b5d13251be37d99f6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-28 13:02:58 +0100

    Delete useless stuff.

--- src/my_4clojure_lab/core.clj
+++
@@ -1,1 +0,0 @@
-(ns my-4clojure-lab.core)
--- test/my_4clojure_lab/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns my-4clojure-lab.test.core
-  (:use [my-4clojure-lab.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
