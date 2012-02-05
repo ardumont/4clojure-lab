b4a3942d35c3bd4a23d8d17f15ff5ec3df8ddb4d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-02-05 22:35:31 +0100

    Update midje.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "My-4clojure-lab - reference my experimentation to solve the problems."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[midje           "1.3.1"]
-                     [lein-midje      "1.0.7"]
+                     [lein-midje      "1.0.8"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]
                      [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
