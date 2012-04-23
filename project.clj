a86d9c5e4b7094dccccf80653e96e92b60fbec3a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-04-23 11:07:44 +0200

    Remove lein plugins as they are declared globally on my machines.

--- project.clj
+++ project.clj
@@ -2,6 +2,4 @@
   :description "My-4clojure-lab - reference my experimentation to solve the problems."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[midje           "1.3.1"]
-                     [lein-midje      "1.0.8"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]
                      [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
