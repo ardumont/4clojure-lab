85e71fceeab6b09bdeae4aac3ca690b1c132d5a9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-27 10:59:00 +0200

    Upgrade to clj 1.4

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
-(defproject my-4clojure-lab "1.0.0-SNAPSHOT"
-  :description "My-4clojure-lab - reference my experimentation to solve the problems."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje           "1.3.1"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
+(defproject my-4clojure-lab/my-4clojure-lab "1.0.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :profiles {:dev
+             {:dependencies
+              [[com.intelie/lazytest
+                "1.0.0-SNAPSHOT"
+                :exclusions
+                [swank-clojure]]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [midje "1.4.0"]]
+  :description "My-4clojure-lab - reference my experimentations to solve the problems.")
