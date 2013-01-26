c9acc02a952248e595fecf08c7439dda603ea1bd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-26 22:39:14 +0100

    stand by problem

--- src/my_4clojure_lab/199/core168.clj
+++ src/my_4clojure_lab/199/core168.clj
@@ -1,8 +1,7 @@
 (ns ^{:doc "http://www.4clojure.com/problem/168#prob-title"}
   my-4clojure-lab.199.core168
-  (:use clojure.repl)
-  (:use clojure.java.javadoc)
-  (:use [midje.sweet]))
+  (:use [clojure.java.javadoc]
+        [midje.sweet :only [fact truthy future-fact]]))
 
 ;; Infinite Matrix
 
@@ -25,17 +24,17 @@
 ;; returns the finite s-by-t matrix that consists of the first t entries of each of the first s rows of the matrix B or,
 ;; equivalently, that consists of the first s entries of each of the first t columns of the matrix B.
 
-(defn infinity
+(defn infinity "For solving the problem."
   [])
 
-(fact
+(future-fact
   (take 5 (map #(take 6 %) (infinity str))) => [["00" "01" "02" "03" "04" "05"]
                                                 ["10" "11" "12" "13" "14" "15"]
                                                 ["20" "21" "22" "23" "24" "25"]
                                                 ["30" "31" "32" "33" "34" "35"]
                                                 ["40" "41" "42" "43" "44" "45"]])
 
-(fact
+(future-fact
   (take 6 (map #(take 5 %) (infinity str 3 2))) => [["32" "33" "34" "35" "36"]
                                                     ["42" "43" "44" "45" "46"]
                                                     ["52" "53" "54" "55" "56"]
@@ -43,31 +42,31 @@
                                                     ["72" "73" "74" "75" "76"]
                                                     ["82" "83" "84" "85" "86"]])
 
-(fact
+(future-fact
   (infinity * 3 5 5 7) => [[15 18 21 24 27 30 33]
                            [20 24 28 32 36 40 44]
                            [25 30 35 40 45 50 55]
                            [30 36 42 48 54 60 66]
                            [35 42 49 56 63 70 77]])
 
-(fact (infinity #(/ % (inc %2)) 1 0 6 4) => [[1/1 1/2 1/3 1/4]
+(future-fact (infinity #(/ % (inc %2)) 1 0 6 4) => [[1/1 1/2 1/3 1/4]
                                              [2/1 2/2 2/3 1/2]
                                              [3/1 3/2 3/3 3/4]
                                              [4/1 4/2 4/3 4/4]
                                              [5/1 5/2 5/3 5/4]
                                              [6/1 6/2 6/3 6/4]])
 
-(fact
+(future-fact
   (= (class (infinity (juxt bit-or bit-xor)))
      (class (infinity (juxt quot mod) 13 21))
      (class (lazy-seq))) => truthy)
 
-(fact
+(future-fact
   (= (class (nth (infinity (constantly 10946)) 34))
      (class (nth (infinity (constantly 0) 5 8) 55))
      (class (lazy-seq))) => truthy)
 
-(fact (let [m 377 n 610 w 987
+(future-fact (let [m 377 n 610 w 987
             check (fn [f s] (every? true? (map-indexed f s)))
             row (take w (nth (infinity vector) m))
             column (take w (map first (infinity vector m n)))
