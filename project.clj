e17f3dca040a043e339e17ae29dee7eb1f9c357d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-26 22:39:04 +0100

    cleaning up the project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,5 @@
 (defproject my-4clojure-lab/my-4clojure-lab "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :profiles {:dev
-             {:dependencies
-              [[com.intelie/lazytest
-                "1.0.0-SNAPSHOT"
-                :exclusions
-                [swank-clojure]]]}}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.4.0"]]
   :description "My-4clojure-lab - reference my experimentations to solve the problems.")
