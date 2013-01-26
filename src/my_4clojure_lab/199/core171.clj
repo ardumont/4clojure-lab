f715220ad949994aaed961b7d73d8e7993282996
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-26 22:39:42 +0100

    171 problem starting up

---
+++ src/my_4clojure_lab/199/core171.clj
@@ -0,0 +1,21 @@
+(ns ^{:doc "http://www.4clojure.com/problem/171#prob-title"}
+  my-4clojure-lab.199.core171
+  (:use [clojure.java.javadoc]
+        [midje.sweet]))
+
+;; Write a function that takes a sequence of integers and returns a sequence of "intervals".
+;; Each interval is a a vector of two integers, start and end, such that all integers between start and end (inclusive)
+;; are contained in the input sequence.
+
+(defn intervals
+  [s])
+
+(future-fact (intervals [1 2 3]) => [[1 3]])
+
+(future-fact (intervals [10 9 8 1 2 3]) => [[1 3] [8 10]])
+
+(future-fact (intervals [1 1 1 1 1 1 1]) => [[1 1]])
+
+(future-fact (intervals []) => [])
+
+(future-fact (intervals [19 4 17 1 3 10 2 13 13 2 16 4 2 15 13 9 6 14 2 11]) => [[1 4] [6 6] [9 11] [13 17] [19 19]])
