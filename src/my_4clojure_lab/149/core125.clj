c53dc5f4eb3a5f445668222b0b752cd8f89a3df8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-02-02 23:06:23 +0100

    tryout on gus' quinundrum

---
+++ src/my_4clojure_lab/149/core125.clj
@@ -0,0 +1,20 @@
+(ns ^{:doc "http://www.4clojure.com/problem/125#prob-title"}
+  my-4clojure-lab.149.core125
+  (:use [midje.sweet]))
+
+;; Gus' Quinundrum
+
+;; Difficulty:	Hard
+;; Topics:	logic fun brain-teaser
+
+;; Create a function of no arguments which returns a string that is an exact copy of the function itself.
+
+;; Hint: read this if you get stuck (this question is harder than it first appears); but it's worth the effort to solve it independently if you can!
+
+;; Fun fact: Gus is the name of the 4Clojure dragon.
+
+;(define function define data)
+
+((fn [x] (str (list x (list (quote quote) x)))) (quote (fn [x] (str (list x (list (quote quote) x))))))
+
+(= (str 's) (s))
