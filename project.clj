735042e6439a4a87c1a3224356f0f14b9ed3e5ba
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-22 12:17:31 +0200

    Updating to latest clojure.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject my-4clojure-lab/my-4clojure-lab "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "My-4clojure-lab - reference my experimentations to solve the problems."
-  :dependencies [[org.clojure/clojure     "1.4.0"]
+  :dependencies [[org.clojure/clojure     "1.5.1"]
                  [org.clojure/tools.trace "0.7.3"]
-                 [midje                   "1.4.0"]])
+                 [midje                   "1.5.1"]])
